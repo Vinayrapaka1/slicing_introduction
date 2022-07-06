bd81a4d2aab3ce56595902235575bb73835e7eb3
VINAY KUMAR RAPAKA <107675897+Vinayrapaka1@users.noreply.github.com>
2022-07-06 15:17:28 +0530

    Add files via upload

---
+++ slicing_introduction.py
@@ -0,0 +1,25 @@
+# x='department'
+# print(x[0]+x[1]+x[2]+x[3]+x[4]+x[5])
+# print(x[6]+x[7]+x[8]+x[9])
+# print(x[0:6])
+
+# y='vinay'
+# print(y[0]+y[1]+y[2])
+# print(y[0:3])
+
+# a='pyhton'
+# print(a[0:])
+# print(a[1:])
+# print(a[4:])
+# print(a[5:])
+
+# print(a[:2])
+# print(a[:4])
+# print(a[:5])
+# print(a[2:4])
+
+# print(a[4:2])
+# print(a[2:2])
+# print(a[3:4])
+# print(a[5:2])
+
